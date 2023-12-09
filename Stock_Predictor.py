c34dfd746a4fd8242b791a928503ed5c4121479d
Suraj9968 <surajsingh24@icloud.com>
2023-12-09 16:21:59 +0530

    chatbot-update

--- Stock_Predictor.py
+++ Stock_Predictor.py
@@ -10,6 +10,7 @@ import cufflinks as cf
 from plotly import graph_objs as go
 import replicate
 import os
+from openai import OpenAI
 
 # App title
 st.set_page_config(page_title="🤑 Stocks-GPT")
@@ -35,15 +36,9 @@ end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
 ticker_list = pd.read_csv('constituents_symbols.txt')
 tickerSymbol = st.sidebar.selectbox('Stock ticker', ticker_list) # Select ticker symbol
 
+
 with st.sidebar:
     st.title('🤑💸💬 Stocks-GPT Chatbot')
-    replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-        st.warning('Please enter your credentials!', icon='⚠️')
-    else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
-
 
 
 
@@ -132,48 +127,25 @@ temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, va
 top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
 max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+st.caption("🚀 Stock-GPT powered by Llama LLM")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
-def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
-        else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
-    return output
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
-# User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
+    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
